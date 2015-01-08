b52fa7d3aaeefd1f13f6e53142d20f5bf0812a57
Dylan Miller <dmiller3199@gmail.com>
2015-01-07 19:15:59 -0800

    1st lecture.

---
+++ lecturestuff/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            class Horse {
+                //class body
+                //class body
+                //class body
+}
+            class Car {
+                //class body
+                //class body
+                //class body
+}
+            class Clown {
+                //class body
+                //class body
+                //class body
+}
+            $sweater1 = new sweater();
+            $sweater2 = new sweater();
+            $sweater3 = new sweater();
+            class sweater1 {
+
+                public $color = "grey";
+                public $size = "large";
+                public $type = "hoodie";
+            }
+             class sweater2 {
+
+                public $color = "white";
+                public $size = "medium";
+                public $type = "hoodless";
+            }
+             class sweater3 {
+
+                public $color = "striped";
+                public $size = "small";
+                public $type = "turtleneck";
+            }
+            $sweater1 = new sweater();
+            print $sweater1­>color;
+            $sweater2 = new sweater();
+            print $sweater2­>color;
+        ?>
+    </body>
+</html>
